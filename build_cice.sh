84cd602aba9613e1cb4ee2a343d00037d9b7a2f2
Christoph Renkl <christoph.renkl@dal.ca>
2021-03-23 09:36:44 -0300

    Add macros and scripts to compile on tikoraluk

---
+++ build_cice.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export MCT_INCDIR=/home/renklc/software/MCT/mct
+export MCT_LIBDIR=/home/renklc/software/MCT/mct
+
+export MPEU_INCDIR=/home/renklc/software/MCT/mpeu
+export MPEU_LIBDIR=/home/renklc/software/MCT/mpeu
+
+./clean_ice.tikoraluk
+./comp_ice.tikoraluk
